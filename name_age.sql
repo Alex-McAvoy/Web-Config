9be757aae6232d7d1dea0482dc320c8a920a3bd9
Alex <931772462@qq.com>
2022-01-29 20:58:28 +0800

    Upload name_age.sql

--- name_age.sql
+++ name_age.sql
@@ -1,33 +1,40 @@
 /*
-Navicat MySQL Data Transfer
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 8.0.13 : Database - test
+*********************************************************************
+*/
 
-Source Server         : 本地连接
-Source Server Version : 80013
-Source Host           : localhost:3306
-Source Database       : test
+/*!40101 SET NAMES utf8 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 80013
-File Encoding         : 65001
+/*!40101 SET SQL_MODE=''*/;
 
-Date: 2021-01-12 16:59:08
-*/
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 
-SET FOREIGN_KEY_CHECKS=0;
+USE `test`;
 
--- ----------------------------
--- Table structure for `name_age`
--- ----------------------------
+/*Table structure for table `name_age` */
+
 DROP TABLE IF EXISTS `name_age`;
-CREATE TABLE `name_age` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` char(255) NOT NULL,
-  `age` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of name_age
--- ----------------------------
-INSERT INTO `name_age` VALUES ('1', 'alex', '50');
-INSERT INTO `name_age` VALUES ('2', 'tom', '22');
+CREATE TABLE `name_age` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `age` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `name_age` */
+
+insert  into `name_age`(`id`,`name`,`age`) values 
+(1,'Alex',23),
+(2,'Tom',13),
+(3,'Jack',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
