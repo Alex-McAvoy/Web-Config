ae65d7e504dea37beffd204c6226a3e5b8f79b23
Alex-McAvoy <931772462@qq.com>
2022-06-24 00:49:54 +0800

    Add files via upload

---
+++ 07.JPA/src/com/jpa/entity/onetomany/Person.java
@@ -0,0 +1,50 @@
+package com.jpa.entity.onetomany;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Description: 一对多关系的实体类
+ * @Author: Alex McAvoy
+ * @Date: 2022/3/11 10:55
+ * @Version: 1.0
+ **/
+@Table(name = "persons")
+@Entity
+public class Person {
+    @GeneratedValue
+    @Id
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    //使用 @OneToMany 映射一对多关系
+    //使用 @JoinColumn 映射外键列的名称
+    @JoinColumn(name = "person_id")
+    @OneToMany
+    private Set<Car> cars = new HashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Set<Car> cars) {
+        this.cars = cars;
+    }
+}
