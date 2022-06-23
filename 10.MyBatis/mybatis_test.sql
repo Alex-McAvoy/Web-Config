66eaa2f45255c469b7e49b64f575ba0edfec4d88
Alex-McAvoy <931772462@qq.com>
2022-06-24 01:36:18 +0800

    Add files via upload

---
+++ 10.MyBatis/mybatis_test.sql
@@ -0,0 +1,179 @@
+/*
+SQLyog Ultimate v12.14 (64 bit)
+MySQL - 8.0.13 : Database - mybatis_test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mybatis_test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+
+USE `mybatis_test`;
+
+/*Table structure for table `car` */
+
+DROP TABLE IF EXISTS `car`;
+
+CREATE TABLE `car` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `car` */
+
+insert  into `car`(`id`,`name`) values 
+(1,'aa'),
+(2,'bb'),
+(3,'cc');
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `department` */
+
+insert  into `department`(`id`,`name`) values 
+(1,'开发部'),
+(2,'测试部');
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `employee` */
+
+insert  into `employee`(`id`,`name`,`gender`,`email`) values 
+(1,'aa','1','AA@163.com'),
+(2,'bb','0','BB@qq.com'),
+(3,'alex','1','22@qq.com'),
+(4,'alex','1','22@qq.com'),
+(5,'alex','1','22@qq.com');
+
+/*Table structure for table `man` */
+
+DROP TABLE IF EXISTS `man`;
+
+CREATE TABLE `man` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `man` */
+
+insert  into `man`(`id`,`name`,`gender`,`email`) values 
+(1,'AA','1','AA@163.com'),
+(2,'BB','0','BB@163.com'),
+(3,'CC','0','CC@163.com');
+
+/*Table structure for table `person` */
+
+DROP TABLE IF EXISTS `person`;
+
+CREATE TABLE `person` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `person` */
+
+insert  into `person`(`id`,`name`,`email`,`gender`) values 
+(1,'XX','XX@163.com','1'),
+(2,'BB','BB@163.com','0'),
+(3,'CC','CC@163.com','1'),
+(4,'ABC','AA@163.com','1');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`gender`,`email`) values 
+(1,'AA','1','AA@163.com'),
+(2,'BB','0','BB@163.com'),
+(3,'CC','1','CC@163.com'),
+(4,'DD','0','DD@163.com'),
+(5,'EE','1','EE@163.com'),
+(6,'FF','1','FF@163.com'),
+(7,'GG','1','GG@163.com'),
+(8,'HH','0','HH@163.com'),
+(9,'II','0','II@163.com');
+
+/*Table structure for table `woman` */
+
+DROP TABLE IF EXISTS `woman`;
+
+CREATE TABLE `woman` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `woman` */
+
+insert  into `woman`(`id`,`name`,`gender`,`email`) values 
+(1,'aa','0','aa@163.com'),
+(2,'bb','0','bb@163.com'),
+(3,'CC','1','CC@163.com');
+
+/*Table structure for table `worker` */
+
+DROP TABLE IF EXISTS `worker`;
+
+CREATE TABLE `worker` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `department_id` int(8) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `worker_department` (`department_id`),
+  CONSTRAINT `worker_department` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `worker` */
+
+insert  into `worker`(`id`,`name`,`department_id`) values 
+(1,'AA',2),
+(2,'BB',1),
+(3,'CC',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
