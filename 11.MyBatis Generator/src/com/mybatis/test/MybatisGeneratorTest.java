5f2a4290a74390fb70b58fa4d86c5fa000121b00
Alex-McAvoy <931772462@qq.com>
2022-06-24 01:37:27 +0800

    Add files via upload

---
+++ 11.MyBatis Generator/src/com/mybatis/test/MybatisGeneratorTest.java
@@ -0,0 +1,39 @@
+package com.mybatis.test;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description: TODO
+ * @Author: Alex McAvoy
+ * @Date: 2022/4/11 22:37
+ * @Version: 1.0
+ **/
+public class MybatisGeneratorTest {
+
+    @Test
+    public void generator() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
