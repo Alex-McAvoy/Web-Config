150a859caf41d14dcb29d90e03b80ab7c23f374c
Alex-McAvoy <931772462@qq.com>
2022-06-25 02:04:17 +0800

    Add files via upload

---
+++ 07.JPA(Hibernate)/src/com/jpa/entity/basic/User.java
@@ -0,0 +1,86 @@
+package com.jpa.entity.basic;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * 注解说明
+ *
+ * @Entity: 令类映射数据表
+ * @Table: 说明映射数据表对应的数据表名
+ * @Id: 声明主键
+ * @GenerateValue: 标注主键的生成策略
+ * @Column: 令属性映射数据表的一个字段，可设置字段的长度、是否为空等属性
+ * @Basic: 对未标注的 getXxx() 方法，默认为该注解，其会自动映射到数据表中的 XXX 字段
+ * @Transient: 若一个属性非数据库表的字段映射，加上该注解后 ORM 框架会忽略该属性
+ * @Temporal: 映射日期、时间的属性时，调整精度
+ **/
+@Cacheable(true)
+@Table(name = "users")
+@Entity
+public class User {
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    private String password;
+    @Temporal(TemporalType.DATE)
+    private Date birth;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+    @Transient
+    private String info;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    //工具方法，不需要映射为数据表的一列
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+}
---
+++ 07.JPA(Hibernate)/src/com/jpa/testpersist/TestPersistence.java
@@ -0,0 +1,266 @@
+package com.jpa.testpersist;
+
+import com.jpa.entity.basic.User;
+import org.hibernate.ejb.QueryHints;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Description: 一对一映射
+ * @Param: 
+ * @Return: 
+ * @Author: Alex McAvoy
+ * @Date: 2022/3/11 10:46
+ **/
+public class TestPersistence {
+    String persistenceUnitName = "TestPersistenceUnit";
+    EntityManagerFactory entityManagerFactory = null;
+    EntityManager entityManager = null;
+    EntityTransaction transaction = null;
+
+    @Before
+    public void init() {
+        //1.创建EntityManagerFactory
+        entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
+        //2.创建EntityManager
+        entityManager = entityManagerFactory.createEntityManager();
+        //3.开启事务
+        transaction = entityManager.getTransaction();
+        transaction.begin();
+    }
+
+    /**
+     * @Description: 类似于 Hibernate 中的 Save 方法，使对象由临时状态变为持久化状态
+     * 但与 save 方法不同是，若对象有 id 则不能执行 insert 操作，会抛出异常
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 18:24
+     **/
+    @Test
+    public void testPersistence() {
+        //4.进行持久化操作
+        User user = new User();
+        user.setName("123");
+        user.setPassword("123");
+        user.setBirth(new Date());
+        user.setCreateTime(new Date());
+        entityManager.persist(user);
+    }
+
+    @After
+    public void destroy() {
+        //5.提交事务
+        transaction.commit();
+        //6.关闭EntityManager
+        entityManager.close();
+        //7.关闭EntityManagerFactory
+        entityManagerFactory.close();
+    }
+
+    /**
+     * @Description: 根据 id 查找数据表的对象，相当于 Hibernate 中 Session 的 get 方法
+     * 调用完毕后即向数据库发 SQL 初始化对象
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 14:23
+     **/
+    @Test
+    public void testFind() {
+        User user = entityManager.find(User.class, 5);
+        System.out.println("-----------------");
+        System.out.println(user);
+    }
+
+    /**
+     * @Description: 根据 id 查找数据表的对象，相当于 Hibernate 中 Session 的 load 方法
+     * 直到真正使用对象时，才向数据库发 SQL 初始化对象
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 14:25
+     **/
+    @Test
+    public void testGetReference() {
+        User user = entityManager.getReference(User.class, 5);
+        System.out.println("-----------------");
+        System.out.println(user);
+    }
+
+    /**
+     * @Description: 类似于 Hibernate 的 delete 方法，但该方法只能移除持久化对象，
+     * 而 delete 方法还可以移除游离对象
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 18:26
+     **/
+    @Test
+    public void testRemove() {
+//        //游离对象
+//        User user = new User();
+//        user.setId(2);
+//        session.delete(user);
+
+        User user = entityManager.find(User.class, 2);
+        entityManager.remove(user);
+    }
+
+    /**
+     * @Description: 类似 Hibernate 的 saveOrUpdate 方法
+     *               1.testMerge1(): 若传入的是临时对象，会创建一个新的对象，并将临时对象的属性复制到新对象中，
+     *                 然后对新的对象进行持久化操作
+     *               2.若传入的是游离对象，即传入的对有 OID，有以下三种情况：
+     *                 1）testMerge2():
+     *                   1.1 在 EntityManager 缓存中没有该对象
+     *                   1.2 若在数据库中也没有对应记录
+     *                   1.3 JPA 会创建一个新对象，然后将当前游离对象属性复刻到新创建的对象中
+     *                   1.4 对新创建的对象执行 insert 操作
+     *                 2）testMerge3():
+     *                   2.1 在 EntityManager 缓存中没有该对象
+     *                   2.2 若在数据库中有对应记录
+     *                   2.3 JPA 会查询对应的记录，然后返回该记录对应的对象
+     *                   2.4 将游离对象的属性复制到查询到的对象中
+     *                   2.5 对查询到的对象执行 update 操作
+     *                 3）testMerge4():
+     *                   3.1 若 EntityManager 缓存中有该对象
+     *                   3.2 JPA 会把游离对象的属性复制到 EntityManager 缓存中的对象
+     *                   3.3 对 EntityManager 缓存中的对象执行 update 操作
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 18:34
+     **/
+    @Test
+    public void testMerge1() {
+        User user1 = new User();
+        user1.setName("123");
+        user1.setPassword("123");
+        user1.setBirth(new Date());
+        user1.setCreateTime(new Date());
+
+        User user2 = entityManager.merge(user1);
+        System.out.println(user1.getId());
+        System.out.println(user2.getId());
+    }
+
+    @Test
+    public void testMerge2() {
+        User user1 = new User();
+        user1.setName("123");
+        user1.setPassword("123");
+        user1.setBirth(new Date());
+        user1.setCreateTime(new Date());
+
+        user1.setId(100);
+
+        User user2 = entityManager.merge(user1);
+        System.out.println(user1.getId());
+        System.out.println(user2.getId());
+    }
+
+    @Test
+    public void testMerge3() {
+        User user1 = new User();
+        user1.setName("456");
+        user1.setPassword("456");
+        user1.setBirth(new Date());
+        user1.setCreateTime(new Date());
+
+        user1.setId(4);
+
+        User user2 = entityManager.merge(user1);
+        System.out.println(user1.getId());
+        System.out.println(user2.getId());
+    }
+
+    @Test
+    public void testMerge4() {
+        User user1 = new User();
+        user1.setName("789");
+        user1.setPassword("789");
+        user1.setBirth(new Date());
+        user1.setCreateTime(new Date());
+
+        user1.setId(5);
+
+        User user2 = entityManager.find(User.class,5);
+        entityManager.merge(user1);
+
+        System.out.println(user1 == user2); //false
+    }
+
+    /**
+     * @Description: 同 Hibernate 中的 flush 方法
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 18:53
+     **/
+    @Test
+    public void testFlush() {
+        User user = entityManager.find(User.class,7);
+        System.out.println(user);
+
+        user.setName("AA");
+        entityManager.flush();
+    }
+
+    /**
+     * @Description: 同 Hibernate 中的 refresh 方法
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/10 18:54
+     **/
+    @Test
+    public void testRefresh() {
+        User user = entityManager.find(User.class,7);
+        user = entityManager.find(User.class,7);
+        
+        entityManager.refresh(user);
+    }
+
+    /**
+     * @Description: 测试二级缓存
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/11 19:02
+     **/
+    @Test
+    public void testSecondLevelCache() {
+        String jpql = "FROM User u";
+        Query query = entityManager.createQuery(jpql);
+        List<User> users = query.setHint(QueryHints.HINT_CACHEABLE,true).getResultList();
+        entityManager.close();
+
+        entityManager = entityManagerFactory.createEntityManager();
+        query = entityManager.createQuery(jpql);
+        users = query.setHint(QueryHints.HINT_CACHEABLE,true).getResultList();
+    }
+
+    /**
+     * @Description: JPQL 测试
+     * @Param: []
+     * @Return: void
+     * @Author: Alex McAvoy
+     * @Date: 2022/3/11 19:10
+     **/
+    @Test
+    public void testJPQL() {
+        String jpql = "FROM User u WHERE u.id > ?";
+        Query query = entityManager.createQuery(jpql);
+
+        //占位符索引从1开始
+        query.setParameter(1,4);
+        List<User> users = query.getResultList();
+        System.out.println(users.size());
+    }
+}
